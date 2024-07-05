eadd5e00f3e4425b6d22bd87bbb1421a14b44c8b
Hugo Carneiro <hcarneiro@weboo.biz>
2024-07-05 13:27:32 +0100

    ID-4531 [FIX] Fixes for checking the correct plan (#129)
    * New setting for linking a registration screen
    
    * Show the signup button and warning
    
    * Translation support
    
    * Added new link reference
    
    * Callout styles
    
    * Update validation and button presence check
    
    * Fixing Codacity issues
    
    * Account for enterprise and free apps
    
    * typo
    
    * Small fix
    
    * Fixes for checking different app types

--- js/build.js
+++ js/build.js
@@ -90,15 +90,17 @@ Fliplet.Widget.instance('login-ds', function(data) {
     return false;
   }
 
-  function checkSignupButton() {
+  function isPublicApp() {
     var organizationPlan = Fliplet.Env.get('organizationPlan');
     var appPlan = Fliplet.Env.get('appPlan');
 
-    isPublicApp = (!appPlan && (!organizationPlan || !organizationPlan.name))
+    return (!appPlan && (!organizationPlan || !organizationPlan.name))
       || (appPlan && !appPlan.active)
       || (appPlan && appPlan.active && (appPlan.name === 'public' || appPlan.name === 'public-plus'));
+  }
 
-    if (Fliplet.Env.get('preview') && isPublicApp && data.registrationAction && data.registrationAction.page && isSignUpButtonHidden()) {
+  function checkSignupButton() {
+    if (Fliplet.Env.get('preview') && isPublicApp() && data.registrationAction && data.registrationAction.page !== 'none' && isSignUpButtonHidden()) {
       $container.find('.signup-warning').removeClass('hidden');
     }
   }
@@ -233,7 +235,7 @@ Fliplet.Widget.instance('login-ds', function(data) {
 
       var _this = $(this);
 
-      if (isPublicApp && isSignUpButtonHidden()) {
+      if (isPublicApp() && data.registrationAction && data.registrationAction.page !== 'none' && isSignUpButtonHidden()) {
         return Fliplet.UI.Toast({
           type: 'regular',
           duration: false,
